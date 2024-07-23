a77b0e40ad1822b4b7ef1e2864af935751a31772
Tatiana Smoliar <tatiana.smoliar@tuhh.de>
2024-07-23 09:38:35 +0200

    linked list project

---
+++ linked_list/ListenElement.h
@@ -0,0 +1,22 @@
+#ifndef _LISTENELEMENT_H_
+#define _LISTENELEMENT_H_
+
+#include "Student.h"
+
+class ListenElement
+{
+private:
+    Student data;
+    ListenElement* next;
+
+public:
+    ListenElement(Student pData, ListenElement* pNext);
+
+    void setData(Student pData);
+    void setNext(ListenElement* pNext);
+
+    Student getData() const;
+    ListenElement* getNext() const;
+};
+
+#endif
